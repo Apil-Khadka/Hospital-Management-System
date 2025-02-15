56270ffcebb3083fce3085325ceb3592e6209dee
Apil Khadka <apilkhadka2020@gmail.com>
2025-02-15 19:04:54 +0545

    controller resource routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,25 @@
 <?php
 
+use App\Http\Controllers\Hospital\AppointmentController;
+use App\Http\Controllers\Hospital\BillController;
+use App\Http\Controllers\Hospital\DepartmentController;
+use App\Http\Controllers\Hospital\LabOrderController;
+use App\Http\Controllers\Hospital\LabTestController;
+use App\Http\Controllers\Hospital\PatientController;
+use App\Http\Controllers\Hospital\PrescriptionController;
+use App\Http\Controllers\Hospital\StaffController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::apiResource('users', UserController::class);
+Route::apiResource('patients', PatientController::class);
+Route::apiResource('prescriptions', PrescriptionController::class);
+Route::apiResource('appointments', AppointmentController::class);
+Route::apiResource('bills', BillController::class);
+Route::apiResource('departments', DepartmentController::class);
+Route::apiResource('lab-orders', LabOrderController::class);
+Route::apiResource('lab-tests', LabTestController::class);
+Route::apiResource('staff', StaffController::class);
